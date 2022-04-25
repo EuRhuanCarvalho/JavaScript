b7df9ea6392f1c4f74e1a76ed7229800139a9b61
ReugenioABD <rhuan_eugenio@hotmail.com>
2022-04-25 15:55:26 -0300

    commit 2

--- js/principal.js
+++ js/principal.js
@@ -21,7 +21,7 @@ if (peso <= 0 || peso >= 1000) {
 }
 
 if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida!");
+console.log("Altura inválida S!");
     alturaEhValida = false;
     tdImc.textContent = "Altura inválida";
 }
